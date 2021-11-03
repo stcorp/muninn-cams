31990c08c9154847f4c72b717fb3afcf72bf4e18
Sander Niemeijer <svniemeijer@mac.com>
2021-11-03 18:34:11 +0100

    Add additional attributes

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 from setuptools import setup
 
 setup(
-    name='muninn-cams',
-    version='1.0',
+    name="muninn-cams",
+    version="1.0",
+    description="Muninn extension for CAMS GRIB products from the ECMWF mars archive",
+    url="https://github.com/stcorp/muninn-cams",
+    author="S[&]T",
     license="BSD",
-    py_modules=['muninn_cams'],
-    install_requires=['muninn', 'muninn_ecmwfmars'],
+    py_modules=["muninn_cams"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        "Environment :: Plugins",
+    ],
+    install_requires=["muninn", "muninn_ecmwfmars"],
 )
